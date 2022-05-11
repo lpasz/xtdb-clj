97063a7b58d466629ad28914624f832f505dbb8d
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-05-11 20:36:33 -0300

    first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject db "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.xtdb/xtdb-core "1.20.0"]
+                 [com.xtdb/xtdb-rocksdb "1.20.0"]]
+  :repl-options {:init-ns db.core})
